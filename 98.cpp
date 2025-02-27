30faa6aae92c937501b27e9fbc7c44143fa7f91c
22BCS16123 <22BCS16123@cuchd.in>
2025-02-27 10:08:57 +0530

    98.cpp

---
+++ 98.cpp
@@ -0,0 +1,36 @@
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+       
+        return isValidBSTHelper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    private boolean isValidBSTHelper(TreeNode node, long min, long max) {
+        
+        if (node == null) {
+            return true;
+        }
+        
+        if (node.val <= min || node.val >= max) {
+            return false;
+        }
+        
+        return isValidBSTHelper(node.left, min, node.val) && 
+               isValidBSTHelper(node.right, node.val, max);
+    }
+}
+
+Input
+root =
+[2,1,3]
+Output
+true
+Expected
+true
